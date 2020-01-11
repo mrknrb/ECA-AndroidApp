57a6a0b8bf3bdacb01d6b9ca58f4369ea93a42c3
mrknrb <mrknrb2@gmail.com>
2020-01-11 20:55:03 +0100

    13 listview menunélkül

--- app/src/main/java/com/example/android/trackmysleepquality/ttsbetoltes.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/ttsbetoltes.kt
@@ -2,99 +2,72 @@ package com.example.android.trackmysleepquality
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.TextView
+import java.util.Arrays.asList
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.example.android.trackmysleepquality.database.BetoltesItem
-import androidx.recyclerview.widget.RecyclerView
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.ContextMenu
+import android.view.View
 
 
 class ttsbetoltes : AppCompatActivity() {
-    private var mRecyclerView: RecyclerView? = null
-    private var mAdapter: BetoltesAdapter? = null
-    private var mLayoutManager: RecyclerView.LayoutManager? = null
-    var mExampleList = mutableListOf<BetoltesItem>()
+
+
+    private lateinit var listView: ListView
+    /*
+    override fun onCreateContextMenu(menu:ContextMenu , v:ListView , menuInfo: AdapterView.AdapterContextMenuInfo) {
+        if (v.getId() == R.id.listview) {
+           var lv:ListView  = v
+           var acmi: AdapterView.AdapterContextMenuInfo  =  menuInfo
+           var obj  =  lv.getItemAtPosition(acmi.position);
+
+            menu.add("One");
+            menu.add("Two");
+            menu.add("Three");
+            menu.add(obj.name);
+        }
+    }
+    */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ttsbetoltes)
 
-        // var exampleList:List<BetoltesItem>?=null
-        // var exampleList:List<BetoltesItem> =
-        val exampleList = mutableListOf<BetoltesItem>()
+        val friendsListView = findViewById<ListView>(R.id.listview)
+        registerForContextMenu(friendsListView);
+        val myFriends = ArrayList(asList("Mark", "Jane", "Sussy", "Jan"))
 
-        exampleList.add(BetoltesItem("1", "111"))
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myFriends)
 
-        exampleList.add(BetoltesItem("4521", "1124521"))
-        exampleList.add(BetoltesItem("42541", "178711"))
-        exampleList.add(BetoltesItem("8731", "1175831"))
-        mRecyclerView = findViewById(R.id.recyclerview)
-        mRecyclerView?.setHasFixedSize(true)
+        friendsListView.setAdapter(arrayAdapter)
 
-        mLayoutManager = LinearLayoutManager(this)
-        mAdapter = BetoltesAdapter(exampleList)
+        friendsListView.setOnItemClickListener(object : AdapterView.OnItemClickListener {
+           override fun onItemClick(adapterView: AdapterView<*>, view: View, i: Int, l: Long) {
 
-        mRecyclerView?.setLayoutManager(mLayoutManager)
-        mRecyclerView?.setAdapter(mAdapter)
-        /*
-        mAdapter.setOnItemClickListener(new BetoltesAdapter.OnItemClickListener{
-            override onItemclick(position:Int){
+                Toast.makeText(applicationContext, "Hello " + myFriends[i], Toast.LENGTH_LONG).show()
 
-        }
-        }
-        )
-*/
+            }
 
 
-        fun buildRecyclerView() {
-            mRecyclerView = findViewById(R.id.recyclerview);
-            mRecyclerView?.setHasFixedSize(true);
-            mLayoutManager = LinearLayoutManager(this);
-            mAdapter = BetoltesAdapter(mExampleList);
-
-            mRecyclerView?.setLayoutManager(mLayoutManager);
-            mRecyclerView?.setAdapter(mAdapter);
-
-            
-
-        }
+        })
+        friendsListView.setOnItemLongClickListener (object : AdapterView.OnItemLongClickListener {
+            override fun onItemLongClick(adapterView: AdapterView<*>, view: View, i: Int, l: Long): Boolean {
 
 
-/*
 
 
-        mRecyclerView = findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ExampleAdapter(exampleList);
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
-        */
+                Toast.makeText(applicationContext, "Csá " + myFriends[i], Toast.LENGTH_LONG).show()
+return true
+            }
 
 
-/*
-listView = findViewById(R.id.listView) as ListView
-textView = findViewById(R.id.textView) as TextView
-listItem = resources.getStringArray(R.array.array_technology)
-val adapter = ArrayAdapter<String>(this,
-        android.R.layout.simple_list_item_1, android.R.id.text1, listItem)
-listView.setAdapter(adapter)
+        })
 
-listView.setOnItemClickListener(object : AdapterView.OnItemClickListener {
-    fun onItemClick(adapterView: AdapterView<*>, view: View, position: Int, l: Long) {
-        // TODO Auto-generated method stub
-        val value = adapter.getItem(position)
-        Toast.makeText(applicationContext, value, Toast.LENGTH_SHORT).show()
 
-    }
-})
-*/
+
     }
 }
