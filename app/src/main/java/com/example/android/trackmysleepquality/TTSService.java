ba354f5f402560629c9aa401627ce94595c15031
mrknrb <mrknrb2@gmail.com>
2020-01-13 23:16:31 +0100

    23

--- app/src/main/java/com/example/android/trackmysleepquality/TTSService.java
+++ app/src/main/java/com/example/android/trackmysleepquality/TTSService.java
@@ -1,5 +1,4 @@
 package com.example.android.trackmysleepquality;
-
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -11,11 +10,10 @@ import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.media.session.MediaSession;
 import android.os.IBinder;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.util.Log;
-
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
-
 import static com.example.android.trackmysleepquality.App.CHANNEL_ID;
 
 
@@ -23,44 +21,13 @@ import static com.example.android.trackmysleepquality.App.CHANNEL_ID;
 public class TTSService extends Service {
     private MediaPlayer mMediaPlayer;
 
-  //  private MediaSession
-/*
-  BroadcastReceiver mybroadcast = new BroadcastReceiver() {
-      //When Event is published, onReceive method is called
-      @Override
-      public void onReceive(Context context, Intent intent) {
-          // TODO Auto-generated method stub
-          Log.i("[BroadcastReceiver]", "MyReceiver");
-
-          if (intent.getAction().equals(Intent.ACTION_MEDIA_BUTTON)) {
-              Log.i("mrknrb", "Mediamrk");
-          }
-          else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-              Log.i("mrknrb", "Screen OFF");
-          }
-
-      }
-  };
-*/
-
-
-
     @Override
     public void onCreate() {
         super.onCreate();
-
-
-/*
-        registerReceiver(mybroadcast, new IntentFilter(Intent.ACTION_MEDIA_BUTTON));
-        registerReceiver(mybroadcast, new IntentFilter(Intent.ACTION_SCREEN_OFF));
-*/
-
     }
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String input = intent.getStringExtra("EXTRA_MESSAGE");
-
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,
                 0, notificationIntent, 0);
@@ -70,12 +37,9 @@ public class TTSService extends Service {
                 .setContentText(input)
                 .setContentIntent(pendingIntent)
                 .build();
-
         startForeground(1, notification);
-
         //do heavy work on a background thread
         //stopSelf();
-
         return START_NOT_STICKY;
     }
 
